c8a3baba04e5f0f8b5e1989869b5e26a48579f61
IsisvLoenen <113013839+IsisvLoenen@users.noreply.github.com>
2024-04-25 12:55:43 +0200

    Update FeatureSelection_multiple_chi.py
    Now performing initial feature selection after splitting the data into train and test

--- FeatureSelection_multiple_chi.py
+++ FeatureSelection_multiple_chi.py
@@ -1,27 +1,35 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import chi2_contingency
+from sklearn.model_selection import train_test_split
 
-# Load data and merge the two dataframes to combine features with subgroup
-data = pd.read_table('Train_call.txt')
-new_data = data.iloc[:,4:] # remove first 4 columns
-new_data = new_data.T # transpose to put features as columns
-
+# Load data
+data = pd.read_table('Train_call.txt')  # Shows the data with samples as rows, so need to transpose
+data = data.T  # Transposes data, so that samples are now rows.
 data_target = pd.read_table('Train_clinical.txt')  # Gives sample with associated subgroup
-data_target = data_target.T
-data_target.columns= data_target.iloc[0,:]   # column names should be the sample
-data_target = data_target.iloc[1:]          # remove first row
-data_target = data_target.T            # transpose again to get the samples as rows
 
-new_data['Subgroup']= data_target.iloc[:,0]
+# Extract predictor and target data
+target = data_target.loc[:,
+         "Subgroup"]  # Isolates the subgroups from samples. We need to convert the subgroups into 0, 1, 2
+new_data_unlabeled = data.iloc[4:, :]  # This is the complete cleaned up dataset
+
+# Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(new_data_unlabeled, target, test_size=0.2,
+                                                    random_state=42)
+
+### INITIAL FEATURE SELECTION ###
+# Combining X_train and y_train again in one dataframe
+y_train_df = pd.DataFrame(y_train)
+train_data = X_train
+train_data['Subgroup'] = list(y_train_df.iloc[:,0])
 
 # Perform Chi-Squared test for each feature (column)
 chi2_results = []
 target_variable = 'Subgroup'
 
-for feature in new_data.columns:
+for feature in train_data.columns:
     if feature != target_variable:  # Check if the feature is categorical
-        contingency_table = pd.crosstab(new_data[feature], new_data[target_variable])
+        contingency_table = pd.crosstab(train_data[feature], train_data[target_variable])
         results = chi2_contingency(contingency_table)
         chi2_results.append({'Feature': feature, 'Chi-Squared': results[0], 'p-value': results[1]})
 
@@ -29,11 +37,11 @@ df_chi = pd.DataFrame(chi2_results) # Save the results into a dataframe
 
 # features = df_chi[df_chi['p-value']<0.01]
 # len(features)
-# --> 304
+# --> 152
 
 # Applying FDR control with Benjamini-Hochberg procedure
 df_chi = df_chi.sort_values(by=['p-value'])  # arranging the results by ascending p-values
-q = 0.05    # setting FDR threshold, allowing for 5% FPs
+q = 0.1    # setting FDR threshold, allowing for 10% FPs
 m = len(df_chi['p-value'])
 critical_values = []
 for i in range(1, m+1):
@@ -53,4 +61,7 @@ df_chi['significant'] = significant
 # Select the significant features
 significant_features = df_chi[df_chi['significant']]
 print(len(significant_features))
-# --> 175
+# --> 10 
+
+features = pd.DataFrame(significant_features['Feature'])
+features.to_csv('output_feature_selection.txt', index= False)
